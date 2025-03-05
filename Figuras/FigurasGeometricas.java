8d28c689e848b2891b680d4a8186551e8e72f906
Tati <taortega@estudiante.uniajc.edu.co>
2025-03-05 17:42:26 +0000

    rectangulo

--- Figuras/FigurasGeometricas.java
+++ Figuras/FigurasGeometricas.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class FigurasGeometricas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la base del rectángulo: ");
+        double base = sc.nextDouble();
+        System.out.print("Ingrese la altura del rectángulo: ");
+        double altura = sc.nextDouble();
+        Rectangulo rectangulo = new Rectangulo(base, altura);
+        System.out.println("Área: " + rectangulo.calcularArea());
+        System.out.println("Perímetro: " + rectangulo.calcularPerimetro());
+        System.out.println("Diagonal: " + rectangulo.calcularDiagonal());
 
-        // Circulo
-        System.out.print("Ingrese el radio del círculo: ");
-        Circulo circulo = new Circulo(sc.nextDouble());
-        System.out.println("Área: " + circulo.calcularArea());
-        System.out.println("Perímetro: " + circulo.calcularPerimetro());
-        System.out.println("Diámetro: " + circulo.calcularDiametro());
-
-        sc.close();
     }
 }
--- Figuras/Rectangulo.java
+++ Figuras/Rectangulo.java
@@ -1,3 +1,31 @@
-public class Rectangulo {
+import java.util.Scanner;
+
+public class Rectangulo 
+{
+
+        double base, altura;
     
-}
+        public Rectangulo(double base, double altura) 
+        {
+            this.base = base;
+            this.altura = altura;
+        }
+    
+        public double calcularArea() 
+        {
+            return base * altura;
+        }
+    
+        public double calcularPerimetro()
+        {
+            return 2 * (base + altura);
+        }
+    
+        public double calcularDiagonal() 
+        {
+            return Math.sqrt(Math.pow(base, 2) + Math.pow(altura, 2));
+        }
+    }
+    
+    
+
